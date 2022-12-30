3127c35c8ecc7968b52d6543d9dc40ddf1377c0a
Kushal Kanungo <kushal.kanungo@metacube.com>
2022-12-30 12:11:28 +0530

    Read Operation in Crud

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'home/about'
+  get '/articles', to: 'articles#index'
+  get "/articles/:id", to: 'articles#show'
+
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
