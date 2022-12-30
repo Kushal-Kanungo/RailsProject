232e616265790c83cb014370359eb57c0af2485d
Kushal Kanungo <kushal.kanungo@metacube.com>
2022-12-30 23:29:42 +0530

    Crud Operations Done

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,12 @@ Rails.application.routes.draw do
   get 'home/index'
   root 'home#index'
   get 'home/about'
-  get '/articles', to: 'articles#index'
-  get "/articles/:id", to: 'articles#show'
+
+  # get '/articles', to: 'articles#index'
+  # get "/articles/:id", to: 'articles#show'
+
+  # ? It makes all convenstional routes for us like create, read, update, delete
+  resources :articles
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
