fb0f90b969168a5577d4380300d63c59bc2268ab
Kushal Kanungo <kushal.kanungo@metacube.com>
2023-01-09 14:50:32 +0530

    has and belongs to many

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_01_09_063823) do
+ActiveRecord::Schema[7.0].define(version: 2023_01_09_084211) do
   create_table "articles", force: :cascade do |t|
     t.string "title"
     t.text "body"
@@ -24,6 +24,19 @@ ActiveRecord::Schema[7.0].define(version: 2023_01_09_063823) do
     t.integer "student_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "courses", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "courses_students", id: false, force: :cascade do |t|
+    t.integer "course_id", null: false
+    t.integer "student_id", null: false
   end
 
   create_table "demos", force: :cascade do |t|
