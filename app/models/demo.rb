436c8adbb52790bca1e4900e625eceec0de0d946
Kushal <kushal.kanungo@metacube.com>
2023-01-25 16:40:28 +0530

    Created a demo and sub_demo model and table

--- app/models/demo.rb
+++ app/models/demo.rb
@@ -1,4 +1,4 @@
 class Demo < ApplicationRecord
-    validates :demo_name, presence: true
-    validates :desc, presence: true
+  # validates :demo_name, presence: true
+  # validates :desc, presence: true
 end
