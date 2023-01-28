281c467d34c6b4e474046ccbae28b3b78d63dbab
KushalKanungo <kushalkhandelwal27@gmail.com>
2023-01-29 01:17:17 +0530

    Updated controller

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -7,9 +7,27 @@ class StudentsController < ApplicationController
 
   def show; end
 
+  def create
+    @student = Student.new(student_params)
+    @student.save
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+  end
+
+  def destroy
+    @student.destroy
+  end
+
   private
 
   def set_student
     @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :email, :age)
   end
 end
