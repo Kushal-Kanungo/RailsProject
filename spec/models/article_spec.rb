28875d3617860e6309282cebc842ff6eddaf275b
KushalKanungo <kushalkhandelwal27@gmail.com>
2023-02-06 20:58:14 +0530

    Loop in testing

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -3,6 +3,7 @@ require 'rails_helper'
 RSpec.describe Article, type: :model do
   context 'when creating an article' do
     # It will create a new method named article which when called return article object
+    # It is provided by the factorybot
     let(:article) { build :article }
     let(:article2) { build :article, title: 'Evil' }
     let(:article3) { build :article, title: article.title }
@@ -15,12 +16,24 @@ RSpec.describe Article, type: :model do
     it 'should not be valid for evil article' do
       expect(article2.valid?).to eq(false)
     end
-
     it 'should give error for duplication' do
       article.save
-
       expect(article3.save).to eq(false)
       expect { article3.save! }.to raise_error(ActiveRecord::RecordInvalid)
+    end
+
+    it 'should create an article' do
+      dummy = create(:article)
+      expect(dummy.valid?).to eq(true)
+    end
+
+    # ? To run a loop in it block
+    it 'should run a loop' do
+      titles = ['Harry potter', 'One Punch man']
+      titles.each do |title_name|
+        dummy = build(:article, body: title_name)
+        expect(dummy.valid?).to eq(true)
+      end
     end
   end
 end
