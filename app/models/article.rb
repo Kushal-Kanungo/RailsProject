cfb7b52ee60149560e3e1a3826a4ab6b9ea58f4f
Kushal <kushal.kanungo@metacube.com>
2023-02-06 18:58:33 +0530

    Exection testing

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,10 +1,11 @@
 class Article < ApplicationRecord
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :body, presence: true, length: { minimum: 10 }
   validate :validate_god
 
   def validate_god
     return unless title == 'Evil'
+
     errors.add(:title, 'is evil')
   end
 end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -5,6 +5,7 @@ RSpec.describe Article, type: :model do
     # It will create a new method named article which when called return article object
     let(:article) { build :article }
     let(:article2) { build :article, title: 'Evil' }
+    let(:article3) { build :article, title: article.title }
 
     # logic we want to perform
     it 'should be valid user with all attributes' do
@@ -13,6 +14,13 @@ RSpec.describe Article, type: :model do
 
     it 'should not be valid for evil article' do
       expect(article2.valid?).to eq(false)
+    end
+
+    it 'should give error for duplication' do
+      article.save
+
+      expect(article3.save).to eq(false)
+      expect { article3.save! }.to raise_error(ActiveRecord::RecordInvalid)
     end
   end
 end
