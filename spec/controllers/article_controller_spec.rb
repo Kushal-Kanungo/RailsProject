339aad89cb879cbc994625ae0d1af6ae8642313a
Kushal <kushal.kanungo@metacube.com>
2023-02-07 12:16:11 +0530

    Added tests for render view and resposne status code

--- spec/controllers/article_controller_spec.rb
+++ spec/controllers/article_controller_spec.rb
@@ -4,9 +4,22 @@ RSpec.describe ArticlesController, type: :controller do
   describe 'GET index' do
     let(:article) { create :article }
     # <================ Testing the get index ==================>
-    it 'assigns @articles' do
+    it 'should assigns @articles' do
+      article2 = create(:article)
       get :index # defining the controller method we want to test
-      expect(assigns(:articles)).to eq([article]) # checking if @articles matches array of article we created
+      expect(assigns(:articles)).to eq([article2, article]) # checking if @articles matches array of article we created
+      expect(response).to render_template('index')
+      expect(response).to have_http_status(:ok)
+    end
+
+    it 'should render the index template' do
+      get :index # defining the controller method we want to test
+      expect(response).to render_template('index')
+    end
+
+    it 'should give status code 200' do
+      get :index # defining the controller method we want to test
+      expect(response).to have_http_status(:ok)
     end
   end
 end
